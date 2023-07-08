6f52e71e0b531ab1e2ecb2fbc89ffdb780bfc2f1
GamEsnitzhel <gamesnitzhel@gmail.com>
2023-07-08 09:07:25 -0700

    changes

--- Source/global/audio/audio.gd
+++ Source/global/audio/audio.gd
@@ -1,5 +1,21 @@
 extends Node2D
 
+@onready var theme_intro = $tb2
+@onready var theme_body = $tb
+
+@onready var sound_win = $winnshiz/AudioStreamPlayer
 
 func _ready():
-	pass
+	theme_intro.play()
+
+func introfinished():
+	theme_body.play()
+
+func PlayerWin() -> void:
+	theme_intro.stream_paused = true
+	theme_body.stream_paused = true
+	sound_win.play();
+	await sound_win.finished
+	theme_intro.stream_paused = false
+	theme_body.stream_paused = false
+	
