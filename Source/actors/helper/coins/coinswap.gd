c6392fc8462e30586fb26a98302eb910c7e15ec2
GamEsnitzhel <gamesnitzhel@gmail.com>
2023-07-09 06:32:48 -0700

    I think the bug is fixed actually now this time

--- Source/actors/helper/coins/coinswap.gd
+++ Source/actors/helper/coins/coinswap.gd
@@ -5,5 +5,5 @@ func _on_body_entered(body):
 	#$playerchecker._on_body_entered(body)
 	Controller.PlayerSwitch()
 	if body is Player:
-		Stats.CollectPurpleCoin();
+		if body.state == body.MovementStates.DEAD or body.state == body.MovementStates.MAX: return;
 		queue_free()
--- Source/actors/helper/coins/purpcoin/purplecoin.gd
+++ Source/actors/helper/coins/purpcoin/purplecoin.gd
@@ -6,6 +6,7 @@ func _ready() -> void:
 
 func _on_body_entered(body):
 	if body is Player or body is Enemy:
+		if body.state == body.MovementStates.DEAD or body.state == body.MovementStates.MAX: return;
 		Stats.CollectPurpleCoin();
 		Audio.PurpleCoin();
 		queue_free();
